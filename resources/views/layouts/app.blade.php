8c5788ab53d4a88c17b27fa3105dbdd3287f0505
stephane0713 <stephaned2205@gmail.com>
2026-01-20 15:24:18 +0100

    fix ui problem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,19 +17,19 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased min-h-svh overflow-hidden">
-    <div class="bg-gray-100 min-h-svh">
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
         @include('layouts.navigation')
 
 
         <!-- Page Heading -->
-        @isset($header)
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endisset
+        {{-- @isset($header)
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+        @endisset --}}
 
         <!-- Page Content -->
         <main>
