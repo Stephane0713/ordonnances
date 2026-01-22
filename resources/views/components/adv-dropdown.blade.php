c7f22c84981be4c7bc2753b8ee6bea9bac2a9ab2
stephane0713 <stephaned2205@gmail.com>
2026-01-22 17:00:39 +0100

    added new dropdown component

---
+++ resources/views/components/adv-dropdown.blade.php
@@ -0,0 +1,49 @@
+@props(['width' => '48', 'contentClasses' => 'py-1 bg-white'])
+
+@php
+    $width = match ($width) {
+        '48' => 'w-48',
+        default => $width,
+    };
+@endphp
+
+<div x-data="menuHandler" @click.outside="open = false" @close.stop="open = false">
+
+    <div @click="openAtMouse($event)">
+        {{ $trigger }}
+    </div>
+
+    <div x-show="open" x-ref="menu" x-transition:enter="transition ease-out duration-200"
+        x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-75" x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-95" {{-- Fixed positioning logic --}}
+        class="fixed z-50 {{ $width }} rounded-md shadow-lg" :style="`left: ${pos.x}px; top: ${pos.y}px;`"
+        @click="open = false">
+        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+            {{ $content }}
+        </div>
+    </div>
+</div>
+
+<script>
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('menuHandler', () => ({
+            open: false,
+            pos: { x: 0, y: 0 },
+
+            init() {
+                this.$refs.table.addEventListener('scroll', () => this.open = false);
+            },
+
+            openAtMouse(e) {
+                this.open = !this.open
+                this.$nextTick(() => {
+                    const { offsetWidth: width, offsetHeight: height } = this.$refs.menu;
+
+                    this.pos.x = e.target.getBoundingClientRect().x - width + 32;
+                    this.pos.y = Math.min(e.target.getBoundingClientRect().y + 32, window.innerHeight - height - 16);
+                });
+            }
+        }));
+    });
+</script>
